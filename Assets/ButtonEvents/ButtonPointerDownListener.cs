3db8afe23f3e0c270e800854d2d6216596e99047
nguyenchinh1704 <phamchinhbtk@gmail.com>
2022-11-14 11:37:27 +0700

    Animation and Even Button

---
+++ Assets/ButtonEvents/ButtonPointerDownListener.cs
@@ -0,0 +1,24 @@
+﻿
+
+using UnityEngine ;
+using UnityEngine.Events ;
+using UnityEngine.EventSystems ;
+using UnityEngine.UI ;
+
+[RequireComponent (typeof(Button))]
+public class ButtonPointerDownListener : MonoBehaviour,IPointerDownHandler {
+
+   public UnityEvent onPointerDown ;
+
+   private Button button ;
+
+   private void Awake () {
+      button = GetComponent<Button> () ;
+   }
+
+   public void OnPointerDown (PointerEventData eventData) {
+      if (button.interactable && !object.ReferenceEquals (onPointerDown, null))
+         onPointerDown.Invoke () ;
+   }
+
+}
---
+++ Assets/UI Game/Script/FixedDistance.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedDistance : MonoBehaviour
+{
+    public GameObject leftOb;
+    public GameObject righOb;
+
+
+}
